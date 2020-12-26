2be6df5749022480820081d586c9918c2d6af850
Ankhbayar Enkhtaivan <mefbayar@gmail.com>
2020-12-26 20:56:56 +0800

    Updates

---
+++ precache-manifest.8ef2d1a82ebd7d053825178612e5ebbf.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2885c45594f3a7b7d24daa286349f90b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0d85f192c860cbad21cf",
+    "url": "/static/css/2.b2ee23ac.chunk.css"
+  },
+  {
+    "revision": "379f18be2d87b8e77fd7",
+    "url": "/static/css/main.9dc3facc.chunk.css"
+  },
+  {
+    "revision": "0d85f192c860cbad21cf",
+    "url": "/static/js/2.24edb019.chunk.js"
+  },
+  {
+    "revision": "379f18be2d87b8e77fd7",
+    "url": "/static/js/main.b19f1a0a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "1fbfd4bcffccb94e8e8a5ea70616b296",
+    "url": "/static/media/lg.1fbfd4bc.woff"
+  },
+  {
+    "revision": "2ec2cb2199d4d881e6a6ad86690f6add",
+    "url": "/static/media/lg.2ec2cb21.svg"
+  },
+  {
+    "revision": "f4292655f93dd12d9b8e4fc067ef2489",
+    "url": "/static/media/lg.f4292655.ttf"
+  }
+]);
